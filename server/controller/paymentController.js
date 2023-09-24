9586b8d5d63e96bfb74626008bb007d883fa249c
SHANKAR JADAV <shankarjadav360@gmail.com>
2023-09-24 21:10:03 +0530

    changes

--- server/controller/paymentController.js
+++ server/controller/paymentController.js
@@ -44,7 +44,7 @@ export const paymentVerification = async (req, res) => {
     });
 
     res.redirect(
-      `http://localhost:5173/paymentsuccess?reference=${razorpay_payment_id}`
+      `https://razor-pay-app.onrender.com/paymentsuccess?reference=${razorpay_payment_id}`
     );
   } else {
     res.status(400).json({
